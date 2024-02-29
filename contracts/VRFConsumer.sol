25912fe6a01d19be05c29337e2909592f3a59a1f
SchneiderNejc <nejc.sch@gmail.com>
2024-02-29 14:08:42 +0100

    data structures

--- contracts/VRFConsumer.sol
+++ contracts/VRFConsumer.sol
@@ -2,4 +2,14 @@
 pragma solidity 0.6.7;
 
 import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
+
+contract VRFConsumer is VRFConsumerBase{
+
+    /// @dev Network: Sepolia
+    address public constant vrfCoordinator = 0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625;
+    address public constant linkToken = 0x779877A7B0D9E8603169DdbD7836e478b4624789;
+
+    bytes32 keyHash;
+    uint fee;
+    uint randomResult;
 }
